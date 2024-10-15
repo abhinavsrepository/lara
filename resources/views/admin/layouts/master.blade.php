750133479346b23b319e2e8726b7a4355aecc710
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-10-15 15:06:56 +0530

    hello

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -6,18 +6,18 @@
   <title>General Dashboard &mdash; Stisla</title>
 
   <!-- General CSS Files -->
-  <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/fontawesome/css/all.min.css')}}">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="assets/modules/jqvmap/dist/jqvmap.min.css">
-  <link rel="stylesheet" href="assets/modules/weather-icon/css/weather-icons.min.css">
-  <link rel="stylesheet" href="assets/modules/weather-icon/css/weather-icons-wind.min.css">
-  <link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/jqvmap/dist/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/weather-icon/css/weather-icons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/weather-icon/css/weather-icons-wind.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/modules/summernote/summernote-bs4.css')}}">
 
   <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/components.css">
+  <link rel="stylesheet" href="{{asset('admin/assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/css/components.css')}}">
 <!-- Start GA -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
 <script>
@@ -992,27 +992,27 @@
   </div>
 
   <!-- General JS Scripts -->
-  <script src="assets/modules/jquery.min.js"></script>
-  <script src="assets/modules/popper.js"></script>
-  <script src="assets/modules/tooltip.js"></script>
-  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-  <script src="assets/modules/moment.min.js"></script>
-  <script src="assets/js/stisla.js"></script>
+  <script src="assets/modules/jquery.min.js')}}"></script>
+  <script src="assets/modules/popper.js')}}"></script>
+  <script src="assets/modules/tooltip.js')}}"></script>
+  <script src="assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+  <script src="assets/modules/moment.min.js')}}"></script>
+  <script src="assets/js/stisla.js')}}"></script>
 
   <!-- JS Libraies -->
-  <script src="assets/modules/simple-weather/jquery.simpleWeather.min.js"></script>
-  <script src="assets/modules/chart.min.js"></script>
-  <script src="assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
-  <script src="assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-  <script src="assets/modules/summernote/summernote-bs4.js"></script>
-  <script src="assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+  <script src="{{asset('admin/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
+  <script src="{{asset('admin/assets/modules/chart.min.js')}}"></script>
+  <script src="{{asset('admin/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+  <script src="{{asset('admin/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+  <script src="{{asset('admin/assets/modules/summernote/summernote-bs4.js')}}"></script>
+  <script src="{{asset('admin/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
 
   <!-- Page Specific JS File -->
-  <script src="assets/js/page/index-0.js"></script>
+  <script src="{{asset('admin/assets/js/page/index-0.js')}}"></script>
 
   <!-- Template JS File -->
-  <script src="assets/js/scripts.js"></script>
-  <script src="assets/js/custom.js"></script>
+  <script src="{{asset('admin/assets/js/scripts.js')}}"></script>
+  <script src="{{asset('admin/assets/js/custom.js')}}"></script>
 </body>
 </html>
